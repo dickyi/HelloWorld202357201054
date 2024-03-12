a2b26586566b786f9512f750957ede15d93d84b4
dickyi <irawandicky300@gmail.com>
2024-03-12 07:43:48 +0700

    add hellowordl

--- src/helloworld202357201054/HelloWorld202357201054.java
+++ src/helloworld202357201054/HelloWorld202357201054.java
@@ -14,7 +14,7 @@ public class HelloWorld202357201054 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Helloworld");
     }
     
 }
