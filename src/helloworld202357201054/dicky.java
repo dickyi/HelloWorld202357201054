2a3278ec92fc9e8e6baa5fc8c75508acac81126c
dickyi <irawandicky300@gmail.com>
2024-03-18 21:00:25 +0700

    second

---
+++ src/helloworld202357201054/dicky.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helloworld202357201054;
+
+/**
+ *
+ * @author Irawa
+ */
+public class dicky {
+    public static void main(String[] args) {
+        System.out.println("hai");
+        System.out.println("hello");
+    }
+}
