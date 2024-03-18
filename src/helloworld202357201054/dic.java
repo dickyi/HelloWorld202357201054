a07a6ce63a05977b340d5e9fe2e68f128a136d27
dickyi <irawandicky300@gmail.com>
2024-03-18 20:47:45 +0700

    dic

---
+++ src/helloworld202357201054/dic.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helloworld202357201054;
+
+/**
+ *
+ * @author Irawa
+ */
+public class dic {
+    public static void main(String[] args) {
+        System.out.println("hai");
+    }
+}
